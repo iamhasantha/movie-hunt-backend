b184a743f3a41dd49ddf941d7771ff5ae189fbe8
Hasantha Kariyawasam <2020cs090@stu.ucsc.cmb.ac.lk>
2024-05-28 11:44:29 +0530

    [Dev] Backend project initialized

---
+++ src/main/java/me/hasantha/movies_hunt/MoviesHuntApplication.java
@@ -0,0 +1,13 @@
+package me.hasantha.movies_hunt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviesHuntApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoviesHuntApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/hasantha/movies_hunt/MoviesHuntApplicationTests.java
@@ -0,0 +1,13 @@
+package me.hasantha.movies_hunt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesHuntApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
